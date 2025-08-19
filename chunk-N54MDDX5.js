544d55ab176899e7fbb8759c9ed65552444ff029
Edwin Nuñez <dweniezenun@gmail.com>
2025-08-20 00:45:43 +0200

    Auto-generated commit

---
+++ chunk-N54MDDX5.js
@@ -0,0 +1,1 @@
+import{Cb as i,Za as t,pb as n,qb as s}from"./chunk-B7TIYGNX.js";var a=class o{static \u0275fac=function(e){return new(e||o)};static \u0275cmp=t({type:o,selectors:[["app-notebooks-view"]],decls:2,vars:0,template:function(e,c){e&1&&(n(0,"p"),i(1,"notebooks-view works!"),s())},encapsulation:2})};export{a as NotebooksView};
